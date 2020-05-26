b525040e9ae010e5ce8ccc462ab4a45f9d93ca7d
alicebizeul <alice.bizeul@epfl.ch>
2020-05-26 11:23:20 -0400

    Update debug.py

--- debug.py
+++ debug.py
@@ -95,16 +95,16 @@ def test():
     images = images[indices]
     labels = labels[indices]
     time_series=True
-    if time_series:
-        print('yes')
-        for i, image in enumerate(images):
-            start_image = image
-            end_image = images[np.random.choice(np.where(labels == (labels[i] + 1) % 10)[0])]
-            interpolation = interpolate_arrays(start_image, end_image, batch_size)
-            yield interpolation + np.random.normal(scale=0.01, size=interpolation.shape)
-    else:
-        for i in range(len(images)//batch_size):
-            yield images[i*batch_size:(i+1)*batch_size] 
+    #if time_series:
+    #    print('yes')
+    #    for i, image in enumerate(images):
+    #        start_image = image
+    #        end_image = images[np.random.choice(np.where(labels == (labels[i] + 1) % 10)[0])]
+    #        interpolation = interpolate_arrays(start_image, end_image, batch_size)
+    #        yield interpolation + np.random.normal(scale=0.01, size=interpolation.shape)
+    #else:
+    #    for i in range(len(images)//batch_size):
+    #        yield images[i*batch_size:(i+1)*batch_size] 
 
 
 
