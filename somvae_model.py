13f0466c0c22805bf20737466a11f623c6a19473
alicebizeul <alice.bizeul@epfl.ch>
2020-05-30 14:39:14 -0400

    Update somvae_model.py

--- somvae_model.py
+++ somvae_model.py
@@ -323,10 +323,10 @@ class SOMVAE:
     @lazy_scope
     def loss_reconstruction(self):
         """Computes the combined reconstruction loss for both reconstructions."""
-        print(self.inputs.shape,self.reconstruction_q)
+        print(self.inputs.shape,self.reconstruction_q.shape)
         loss_rec_mse_zq = loss_mse(self.inputs, self.reconstruction_q)
         loss_rec_mse_ze = loss_mse(self.inputs, self.reconstruction_e)
-        print(loss_rec_mse_ze)
+        print('new loss impl',loss_rec_mse_ze.shape)
         loss_rec_mse = loss_rec_mse_zq + loss_rec_mse_ze
         #tf.compat.v1.summary.scalar("loss_reconstruction", loss_rec_mse)
         return loss_rec_mse
@@ -378,10 +378,10 @@ class SOMVAE:
     @lazy_scope
     def loss(self):
         """Aggregates the loss terms into the total loss."""
-        print(self.loss_reconstruction())
-        print(self.loss_commit())
-        print(self.loss_probabilities())
-        print(self.loss_som())
+        print(self.loss_reconstruction().shape)
+        print(self.loss_commit().shape)
+        print(self.loss_probabilities().shape)
+        print(self.loss_som().shape)
         loss = (self.loss_reconstruction() + self.alpha*self.loss_commit() + self.beta*self.loss_som()
                 + self.gamma*self.loss_probabilities() + self.tau*self.loss_z_prob())
         #tf.compat.v1.summary.scalar("loss", loss)
