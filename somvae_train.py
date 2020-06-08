4d489c0edbbeac2e0d7bfe44245e01c5bd78f493
alicebizeul <alice.bizeul@epfl.ch>
2020-06-08 12:14:13 -0400

    Update somvae_train.py

--- somvae_train.py
+++ somvae_train.py
@@ -253,8 +253,8 @@ def train_model(model, lr_val, num_epochs, patience, batch_size, logdir,
     except KeyboardInterrupt:
         pass
     finally:
-        model.save(savefolder.joinpath('model.h5'))
-        tmp = tf.keras.models.load_model(savefolder.joinpath('model.h5'))
+        model.save('../models/model.h5')
+        tmp = tf.keras.models.load_model('../models/model.h5')
         if interactive:
             pbar.close()
 
