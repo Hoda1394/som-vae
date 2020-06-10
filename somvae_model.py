3ca2cdaf011534e288119f30658bd7528eb3163f
alicebizeul <alice.bizeul@epfl.ch>
2020-06-10 13:01:07 -0400

    only reconst

--- somvae_model.py
+++ somvae_model.py
@@ -354,7 +354,7 @@ class SOMVAE(tf.keras.Model):
     #@lazy_scope
     def loss(self):
         """Aggregates the loss terms into the total loss.""" 
-        loss = (self.loss_reconstruction() + self.alpha*self.loss_commit() + self.beta*self.loss_som())
+        loss = (self.loss_reconstruction()) #+ self.alpha*self.loss_commit() + self.beta*self.loss_som())
         #        + self.gamma*self.loss_probabilities() + self.tau*self.loss_z_prob())
         return loss
 
