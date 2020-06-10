5669e38f7a10a965bbcbb53c87666e959331a502
alicebizeul <alice.bizeul@epfl.ch>
2020-06-10 15:16:35 -0400

    Update somvae_train.py

--- somvae_train.py
+++ somvae_train.py
@@ -256,6 +256,8 @@ def train_model(model, lr_val, num_epochs, patience, batch_size, logdir,
 
                 step += 1
                 batch_train = next(train_gen)
+                print(batch_train[1,:,:])
+                print(batch_train.max())
 
                 train_loss= call_train_step(batch_train)
                 #train_loss_prob= call_train_step_prob(batch_train)
