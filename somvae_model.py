17ef2c4ce52eb7dcb0147ce24846f51b641b1426
alicebizeul <alice.bizeul@epfl.ch>
2020-06-09 21:04:15 -0400

    debug

--- somvae_model.py
+++ somvae_model.py
@@ -353,8 +353,8 @@ class SOMVAE(tf.keras.Model):
     #@lazy_scope
     def loss(self):
         """Aggregates the loss terms into the total loss.""" 
-        loss = (self.loss_reconstruction() + self.alpha*self.loss_commit() + self.beta*self.loss_som()
-                + self.gamma*self.loss_probabilities() + self.tau*self.loss_z_prob())
+        loss = (self.loss_reconstruction() + self.alpha*self.loss_commit() + self.beta*self.loss_som())
+        #        + self.gamma*self.loss_probabilities() + self.tau*self.loss_z_prob())
         return loss
 
     @lazy_scope
--- somvae_train.py
+++ somvae_train.py
@@ -221,7 +221,7 @@ def train_model(model, lr_val, num_epochs, patience, batch_size, logdir,
     @tf.function
     def call_train_step(inputs):
         loss = train_step(inputs)
-        loss_prob = train_step_prob(inputs)
+        #loss_prob = train_step_prob(inputs)
         return loss
     
     #@tf.function
