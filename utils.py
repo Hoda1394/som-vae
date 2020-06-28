8d0d06505ce355a5b0d23098f75e472375fb057b
alicebizeul <alice.bizeul@epfl.ch>
2020-06-28 14:50:20 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -222,7 +222,7 @@ def get_dataset(tfrecords_folder,epoch_size,batch_size):
         dataset = dataset.shuffle(buffer_size=20)
 
         #dataset = dataset.map(lambda x: tf.py_function(epoch,[x,epoch_size],[]))
-        dataset = dataset.map(lambda x: epoch(x,epoch_size))
+        #dataset = dataset.map(lambda x: epoch(x,epoch_size))
         dataset = dataset.unbatch()
         dataset = dataset.batch(batch_size,drop_remainder=True)
         dataset = dataset.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
