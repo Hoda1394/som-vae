2de86746bb1b51b5d74410eea4b772b7c3bfc2c5
alicebizeul <alice.bizeul@epfl.ch>
2020-06-28 14:25:58 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -198,7 +198,7 @@ def epoch(sample,batch_size):
     #if sample.shape[0]%batch_size != 0: print('Batch size does not suit scan duration, excess data will be discarded')
     sample = tf.convert_to_tensor(sample)
     print(sample.shape,tf.shape(sample))
-    series_shape = np.asarray(tf.shape(sample),dtype=np.int32)
+    series_shape = np.asarray(tf.shape(sample))
     block_shape = np.asarray([batch_size,series_shape[1]],dtype=np.int32)
     num_blocks = series_shape // block_shape
     sample = tf.reshape(sample, np.insert(block_shape,0,num_blocks[0]))
