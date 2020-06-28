0544095805633b9cf4aa9763cb6de56e398f0b03
alicebizeul <alice.bizeul@epfl.ch>
2020-06-28 16:25:29 -0400

    flatten

--- somvae_model.py
+++ somvae_model.py
@@ -130,7 +130,8 @@ class SOMVAE(tf.keras.Model):
     def get_encoder(self):
         if not self.mnist:
             h_0 = tf.keras.layers.Input(shape=[None,self.input_length, self.input_channels, 1], name='input')
-            h_1 = tf.keras.layers.Dense(256, activation="relu")(h_0)
+            h_flat = tf.keras.layers.Flatten()(h_0)
+            h_1 = tf.keras.layers.Dense(256, activation="relu")(h_flat)
             h_2 = tf.keras.layers.Dense(128, activation="relu")(h_1)
             z_e = tf.keras.layers.Dense(self.latent_dim, activation="relu")(h_2)
 
